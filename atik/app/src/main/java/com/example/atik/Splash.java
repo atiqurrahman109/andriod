3a5f032c33de339d9e5b7aa31497eec2ef8965d9
atiqurrahman109 <atiqurrahman.ar001@gmail.com>
2025-09-03 18:55:17 +0600

    ad

--- atik/app/src/main/java/com/example/atik/Splash.java
+++ atik/app/src/main/java/com/example/atik/Splash.java
@@ -1,6 +1,9 @@
 package com.example.atik;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,24 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Splash extends AppCompatActivity {
 
+    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), home.class);
+                startActivity(intent);
+            }
         });
+
+
     }
 }
